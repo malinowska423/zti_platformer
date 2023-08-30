97bf469c0c5e42218477a9973fd4db2399baa560
malinowska423 <amalinowska423@gmail.com>
2023-08-30 21:21:01 +0200

    state machines

--- src/entities/Hero.js
+++ src/entities/Hero.js
@@ -1,65 +1,124 @@
-import Phaser from 'phaser';
+import Phaser from 'phaser'
+import StateMachine from 'javascript-state-machine'
 
 class Hero extends Phaser.GameObjects.Sprite {
-
   constructor(scene, x, y) {
-    super(scene, x, y, 'hero-run-sheet', 0);
+    super(scene, x, y, 'hero-run-sheet', 0)
 
-    scene.add.existing(this);
-    scene.physics.add.existing(this);
+    scene.add.existing(this)
+    scene.physics.add.existing(this)
 
-    this.anims.play('hero-running');
+    this.anims.play('hero-running')
 
-    this.body.setCollideWorldBounds(true);
-    this.body.setSize(12, 40);
-    this.body.setOffset(12, 23);
-    this.body.setMaxVelocity(250, 400);
+    this.body.setCollideWorldBounds(true)
+    this.body.setSize(12, 40)
+    this.body.setOffset(12, 23)
+    this.body.setMaxVelocity(250, 400)
     this.body.setDragX(750)
 
-    this.keys = scene.cursorKeys;
+    this.keys = scene.cursorKeys
+    this.input = {}
+
+    this.setupAnimations()
+    this.setupMovement()
+  }
+
+  setupAnimations() {
+    this.animState = new StateMachine({
+      init: 'idle',
+      transitions: [
+        { name: 'idle', from: ['falling', 'running', 'pivoting'], to: 'idle' },
+        { name: 'run', from: ['falling', 'idle', 'pivoting'], to: 'running' },
+        { name: 'pivot', from: ['falling', 'running'], to: 'pivoting' },
+        { name: 'jump', from: ['idle', 'running', 'pivoting'], to: 'jumping' },
+        { name: 'flip', from: ['jumping', 'falling'], to: 'flipping' },
+        { name: 'fall', from: '*', to: 'falling' },
+      ],
+      methods: {
+        onEnterState: (lifecycle) => this.anims.play(`hero-${lifecycle.to}`),
+      },
+    })
+
+    this.animPredicates = {
+      idle: () => this.body.onFloor() && this.body.velocity.x === 0,
+      run: () =>
+        this.body.onFloor() &&
+        Math.sign(this.body.velocity.x) === (this.flipX ? -1 : 1),
+      pivot: () =>
+        this.body.onFloor() &&
+        Math.sign(this.body.velocity.x) === (this.flipX ? 1 : -1),
+      jump: () => this.body.velocity.y < 0,
+      flip: () => this.body.velocity.y < 0 && this.moveState.is('flipping'),
+      fall: () => this.body.velocity.y > 0,
+    }
+  }
+
+  setupMovement() {
+    this.moveState = new StateMachine({
+      init: 'standing',
+      transitions: [
+        { name: 'jump', from: 'standing', to: 'jumping' },
+        { name: 'flip', from: 'jumping', to: 'flipping' },
+        { name: 'fall', from: 'standing', to: 'falling' },
+        {
+          name: 'touchdown',
+          from: ['jumping', 'flipping', 'falling'],
+          to: 'standing',
+        },
+      ],
+      methods: {
+        onJump: () => this.body.setVelocityY(-400),
+        onFlip: () => this.body.setVelocityY(-300),
+      },
+    })
+
+    this.movePredicates = {
+      jump: () => this.input.didPressJump,
+      flip: () => this.input.didPressJump,
+      fall: () => !this.body.onFloor(),
+      touchdown: () => this.body.onFloor(),
+    }
   }
 
   preUpdate(time, delta) {
-    super.preUpdate(time, delta);
+    super.preUpdate(time, delta)
+
+    this.input.didPressJump = Phaser.Input.Keyboard.JustDown(this.keys.up)
 
     if (this.keys.left.isDown) {
-      this.body.setAccelerationX(-1000);
-      this.setFlipX(true);
-      this.body.offset.x = 8;
+      this.body.setAccelerationX(-1000)
+      this.setFlipX(true)
+      this.body.offset.x = 8
     } else if (this.keys.right.isDown) {
-      this.body.setAccelerationX(1000);
-      this.setFlipX(false);
-      this.body.offset.x = 12;
+      this.body.setAccelerationX(1000)
+      this.setFlipX(false)
+      this.body.offset.x = 12
     } else {
-      this.body.setAccelerationX(0);
+      this.body.setAccelerationX(0)
     }
 
-    if (this.body.onFloor()) {
-      this.canDoubleJump = false;
+    if (
+      (this.moveState.is('jumping') || this.moveState.is('flipping')) &&
+      !this.keys.up.isDown &&
+      this.body.velocity.y < -150
+    ) {
+      this.body.setVelocityY(-150)
     }
 
-    if (this.body.velocity.y > 0) {
-      this.isJumping = false;
-    }
-
-    const didPressJump = Phaser.Input.Keyboard.JustDown(this.keys.up);
-
-    if (didPressJump) {
-      if (this.body.onFloor()) {
-        this.isJumping = true;
-        this.canDoubleJump = true;
-        this.body.setVelocityY(-400);
-      } else if (this.canDoubleJump) {
-        this.isJumping = true;
-        this.canDoubleJump = false;
-        this.body.setVelocityY(-300);
+    for (const t of this.moveState.transitions()) {
+      if (t in this.movePredicates && this.movePredicates[t]()) {
+        this.moveState[t]()
+        break
       }
     }
 
-    if (!this.keys.up.isDown && this.body.velocity.y < -150 && this.isJumping) {
-      this.body.setVelocityY(-150);
+    for (const t of this.animState.transitions()) {
+      if (t in this.animPredicates && this.animPredicates[t]()) {
+        this.animState[t]()
+        break
+      }
     }
   }
 }
 
-export default Hero;
+export default Hero
--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,38 +1,82 @@
-import Phaser from 'phaser';
-import Hero from '../entities/Hero';
+import Phaser from 'phaser'
+import Hero from '../entities/Hero'
 
 class Game extends Phaser.Scene {
   constructor() {
-    super({key: 'GameScene'});
+    super({ key: 'GameScene' })
   }
 
-  init(data) {
-  }
+  init(data) {}
 
   preload() {
-    this.load.spritesheet('hero-run-sheet', 'assets/hero/run.png', {
-      frameWidth: 32,
-      frameHeight: 64,
-    });
+    const sprites = ['idle', 'run', 'pivot', 'jump', 'flip', 'fall']
+    sprites.forEach((sprite) =>
+      this.load.spritesheet(
+        `hero-${sprite}-sheet`,
+        `assets/hero/${sprite}.png`,
+        {
+          frameWidth: 32,
+          frameHeight: 64,
+        }
+      )
+    )
   }
 
   create(data) {
+    this.cursorKeys = this.input.keyboard.createCursorKeys()
 
-    this.cursorKeys = this.input.keyboard.createCursorKeys();
+    const anims = {
+      idle: {
+        animation: 'idle',
+      },
+      run: {
+        animation: 'running',
+        props: {
+          frameRate: 10,
+          repeat: -1,
+        },
+      },
+      pivot: {
+        animation: 'pivoting',
+      },
+      jump: {
+        animation: 'jumping',
+        props: {
+          frameRate: 10,
+          repeat: -1,
+        },
+      },
+      flip: {
+        animation: 'flipping',
+        props: {
+          frameRate: 30,
+          repeat: 0,
+        },
+      },
+      fall: {
+        animation: 'falling',
+        props: {
+          frameRate: 10,
+          repeat: -1,
+        },
+      },
+    }
+    Object.keys(anims).forEach((anim) =>
+      this.anims.create({
+        key: `hero-${anims[anim].animation}`,
+        frames: this.anims.generateFrameNumbers(`hero-${anim}-sheet`, {}),
+        ...anims[anim].props,
+      })
+    )
 
-    this.anims.create({
-      key: 'hero-running',
-      frames: this.anims.generateFrameNumbers('hero-run-sheet', {}),
-      frameRate: 10,
-      repeat: -1
-    })
+    this.hero = new Hero(this, 250, 160)
 
-    this.hero = new Hero(this, 250, 160);
-
+    const platform = this.add.rectangle(220, 240, 260, 10, 0x4bcb7c)
+    this.physics.add.existing(platform, true)
+    this.physics.add.collider(this.hero, platform)
   }
 
-  update(time, delta) {
-  }
+  update(time, delta) {}
 }
 
-export default Game;
+export default Game
